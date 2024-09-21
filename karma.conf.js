3df8be0acfc0ca1456c9493cf03ff40b9e9967ca
Serhii Serdiuk <130998461+SerhiiSerdiukDev@users.noreply.github.com>
2024-09-21 23:25:14 +0300

    Add a test coverage check

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,28 @@
 module.exports = function (config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine"],
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
     files: ["src/**/*.spec.ts"],
     exclude: [],
-    preprocessors: {},
-    reporters: ["progress"],
+    preprocessors: {
+      "src/**/*.spec.ts": ["coverage"],
+    },
+    reporters: ["progress", "coverage"],
+    coverageReporter: {
+      dir: "coverage",
+      reporters: [
+        { type: "html", subdir: "report-html" },
+        { type: "lcov", subdir: "report-lcov" },
+        { type: "text-summary" },
+      ],
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
