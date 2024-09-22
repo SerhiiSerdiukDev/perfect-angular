9047ec16ce1f509f07ecf03857f189c2fac449e2
Serhii Serdiuk <130998461+SerhiiSerdiukDev@users.noreply.github.com>
2024-09-22 21:53:13 +0300

    Show firebase project ID to fix the action log

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,7 +6,6 @@ declare interface Env {
   readonly NG_APP_RECAPTCHA_SITE_KEY: string;
   readonly NG_APP_FIREBASE_API_KEY: string;
   readonly NG_APP_FIREBASE_AUTH_DOMAIN: string;
-  readonly NG_APP_FIREBASE_PROJECT_ID: string;
   readonly NG_APP_FIREBASE_STORAGE_BUCKET: string;
   readonly NG_APP_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly NG_APP_FIREBASE_APP_ID: string;
